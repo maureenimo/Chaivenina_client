5d65acc9217a6384a3f53af2d7cad9e0954cbd4a
maureen <maureen.murimi@moringaschool.com>
2024-07-02 12:23:07 +0300

    Refactored code and  Added Basic Register Component with State Management

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-import Carousel1 from "../assets/dineinphoto.jpg";
 
 const Register = () => {
   const [user, setUser] = useState({
@@ -13,14 +10,52 @@ const Register = () => {
     phone: "",
   });
 
-  const [registrationSuccess, setRegistrationSuccess] = useState(false);
-
-  const navigate = useNavigate();
-  const image = [Carousel1];
-
   const handleChange = (e) => {
     const id = e.target.id;
     const value = e.target.value;
 
     setUser({ ...user, [id]: value });
   };
+
+  return (
+    <div className="offset-lg-3 col-lg-6">
+      <div className="card">
+        <div className="card-header">
+          <h1>Sign up</h1>
+        </div>
+        <div style={{ marginTop: "20%", borderRadius: "0" }} className="form-dialogue">
+          <form className="container">
+            <div className="row">
+              {["First name", "Last name", "Email", "Phone", "Password"].map(
+                (field) => (
+                  <div key={field} className="col-lg-6">
+                    <div className="form-group">
+                      <label>
+                        {field.charAt(0).toUpperCase() + field.slice(1)}{" "}
+                        <span className="errmsg">*</span>
+                      </label>
+                      <input
+                        type={field === "password" ? "password" : "text"}
+                        value={user[field]}
+                        onChange={handleChange}
+                        name={field}
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+                )
+              )}
+            </div>
+            <div className="form-dialogue">
+              <button type="submit" className="continue-shopping">
+                Signup
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
