b8d876a414d2c6b67f5450ab5ffe5e39187133cf
maureen <maureen.murimi@moringaschool.com>
2024-07-03 17:45:50 +0300

    Integrated the ItemActions component for managing individual cart items and Enhanced the Cart UI.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import ItemActions from "./ItemActions";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
@@ -11,8 +12,36 @@ function Cart() {
     savedCart ? JSON.parse(savedCart) : []
   );
 
+  // const location = useLocation();
+  // const cart = location.state ? location.state.cart : [];
+
+  // useEffect(() => {
+  //   console.log("Cart items:", cart);
+  //   if (JSON.stringify(cart) !== JSON.stringify(cartItems)) {
+  //     setCartItems(cart);
+  //   }
+  // }, [cart]);
+
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  useEffect(() => {
+    const calculateTotal = () => {
+      let newSubtotal = 0;
+
+      for (let i = 0; i < cartItems.length; i++) {
+        const price = parseFloat(cartItems[i].price);
+        const quantity = cartItems[i].quantity;
+
+        newSubtotal += price * quantity;
+      }
+
+      setTotal(newSubtotal.toFixed(2));
+      setSubtotal(newSubtotal.toFixed(2));
+    };
+
+    calculateTotal();
   }, [cartItems]);
 
   const navigate = useNavigate();
@@ -26,38 +55,35 @@ function Cart() {
       navigate("/sign_up");
     }
   }
-  useEffect(() => {
-    const calculateTotal = () => {
-      let newSubtotal = 0;
-  
-      for (let i = 0; i < cartItems.length; i++) {
-        const price = parseFloat(cartItems[i].price);
-        const quantity = cartItems[i].quantity;
-  
-        newSubtotal += price * quantity;
-      }
-  
-      setTotal(newSubtotal.toFixed(2));
-      setSubtotal(newSubtotal.toFixed(2));
-    };
-  
-    calculateTotal();
-  }, [cartItems]);
-  
+
   return (
-    <div>
+  <div>
       <Navbar />
       <div className="cart-page">
         <div className="cart-container">
-          <h2>MY CART</h2>
+        <h2>MY CART</h2>
           {cartItems.length === 0 ? (
             <p>Your cart is empty.</p>
           ) : (
             cartItems.map((item) => (
               <div key={item.id} className="cart-item">
+                <div className="item-image">
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className="cart-image"
+                  />
+                </div>
                 <div className="item-details">
                   <div className="item-name">Item: {item.name}</div>
+                  <div className="item-description">{item.description}</div>
+                  <br />
                   <div className="item-price">Kshs {item.price}</div>
+                  <ItemActions
+                    item={item}
+                    cartItems={cartItems}
+                    setCartItems={setCartItems}
+                  />
                 </div>
               </div>
             ))
@@ -80,10 +106,15 @@ function Cart() {
             * Delivery charges will be applicable based on your chosen address
           </div>
           <div className="payment-options">
+          <Link to="/menu">
+            <button className="continue-shopping">Continue Shopping</button>
+          </Link>
+          <Link to="/sign_up">
             <button className="continue-shopping" onClick={handlePayNow}>
               Sign in and Pay
             </button>
-          </div>
+          </Link>
+        </div>
         </div>
       </div>
       <Footer />
