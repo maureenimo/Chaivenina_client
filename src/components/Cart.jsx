821d3680d95642d184ff559425a272aad6283d26
maureen <maureen.murimi@moringaschool.com>
2024-07-03 17:41:10 +0300

    Initialized the Cart component with basic state management.

---
+++ src/components/Cart.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+function Cart() {
+  const savedCart = localStorage.getItem("cart");
+  const [subtotal, setSubtotal] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [cartItems, setCartItems] = useState(
+    savedCart ? JSON.parse(savedCart) : []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  const navigate = useNavigate();
+
+  function handlePayNow() {
+    const isLoggedIn = true;
+
+    if (isLoggedIn) {
+      navigate("/payment");
+    } else {
+      navigate("/sign_up");
+    }
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <div className="cart-page">
+        <div className="cart-container">
+          <h2>MY CART</h2>
+          {cartItems.length === 0 ? (
+            <p>Your cart is empty.</p>
+          ) : (
+            cartItems.map((item) => (
+              <div key={item.id} className="cart-item">
+                <div className="item-details">
+                  <div className="item-name">Item: {item.name}</div>
+                  <div className="item-price">Kshs {item.price}</div>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
+        <div className="cart-summary">
+          <div className="summary-item">
+            <span className="summary-label">Sub total:</span>
+            <span className="summary-value">{subtotal}</span>
+          </div>
+          <div className="summary-item">
+            <span className="summary-label">Delivery charges:</span>
+            <span className="summary-value">N/A</span>
+          </div>
+          <div className="summary-item">
+            <span className="summary-label">TOTAL:</span>
+            <span className="summary-value">{total}</span>
+          </div>
+          <div className="summary-note">
+            * Delivery charges will be applicable based on your chosen address
+          </div>
+          <div className="payment-options">
+            <button className="continue-shopping" onClick={handlePayNow}>
+              Sign in and Pay
+            </button>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Cart;
