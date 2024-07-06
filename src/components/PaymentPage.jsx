739dadeb77f6cd5ce86d970dded2139492bfefb8
maureen <maureen.murimi@moringaschool.com>
2024-07-06 20:36:08 +0300

    Enhanced UI

--- src/components/PaymentPage.jsx
+++ src/components/PaymentPage.jsx
@@ -18,7 +18,6 @@ const PaymentPage = () => {
       .then((response) => response.json())
       .then((data) => {
         setLocations(data);
-        // console.log(data);
       })
       .catch((error) => console.error('Error:', error));
   }, []);
@@ -28,29 +27,22 @@ const PaymentPage = () => {
 
     if (selectedPayment === 'mpesa') {
       navigate('/mpesa_payment');
-      //alert('Please make payment to Mpesa Till Number 707070.');
     } else if (selectedPayment === 'cash') {
       alert('Please make payment upon delivery.');
     } else if (selectedPayment === 'paypal') {
       window.location.href = 'https://www.paypal.com/signin';
     } else if (selectedPayment === 'binance') {
-      window.location.href ='https://accounts.binance.com/en/login?gclid=EAIaIQobChMI7ZvOsvOZgwMVfopoCR06AwmyEAAYASAAEgI42_D_BwE&ref=804491327';
+      window.location.href = 'https://accounts.binance.com/en/login?ref=804491327';
     } else if (selectedPayment === 'visa') {
       window.location.href = 'https://www.visaonline.com/login/';
     }
     console.log('Payment submitted');
     alert('Address submitted');
 
-
     const data = new FormData(e.target);
     const formObject = Object.fromEntries(data.entries());
-    // calculate expected delivery time using distance calculated from Google Maps API
     const origin = [-1.3063965601181275, 36.85755932273569];
-    // Get destination logituedesfrom locations(array of objects) based on selected location
-    const destination = locations.find((location) => {
-      // eslint-disable-next-line eqeqeq
-      return location.id == parseInt(formObject.cityId);
-    });
+    const destination = locations.find((location) => location.id == parseInt(formObject.cityId));
 
     console.log(destination);
     const order = {
@@ -72,9 +64,7 @@ const PaymentPage = () => {
       },
     };
     console.log(JSON.stringify(order));
-    // Navigate to tracking page with order details as props
     navigate('/mpesa_payment', { replace: false, state: order });
-
   };
 
   const handlePaymentChange = (e) => {
@@ -83,145 +73,144 @@ const PaymentPage = () => {
 
   return (
     <div>
-    {<Navbar />}
-    <div className="payment-container" style={{backgroundColor: "#ff9d5723"}}>
-      <h2>Payment Details</h2>
-      <form onSubmit={handleSubmit}>
-      <div className="payment-container">
-          <div className="card-payment1">
-            <strong>Payment Options</strong>
-            <br />
-            <div className="payment-options">
-              {['mpesa', 'cash', 'paypal', 'visa', 'binance'].map(
-                (paymentOption) => (
-                  <div key={paymentOption} className="other-option">
-                    <input
-                      type="radio"
-                      id={paymentOption}
-                      name="paymentMethod"
-                      value={paymentOption}
-                      checked={selectedPayment === paymentOption}
-                      onChange={handlePaymentChange}
-                      required
-                    />
-                    <label htmlFor={paymentOption}>
-                      <img
-                        src={getImageSource(paymentOption)}
-                        alt={paymentOption}
+      <Navbar />
+      <div className="payment-container" style={{backgroundColor: "#ff9d5723"}}>
+        <h2>Payment Details</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="payment-container">
+            <div className="card-payment1">
+              <strong>Payment Options</strong>
+              <br />
+              <div className="payment-options">
+                {['mpesa', 'cash', 'paypal', 'visa', 'binance'].map(
+                  (paymentOption) => (
+                    <div key={paymentOption} className="other-option">
+                      <input
+                        type="radio"
+                        id={paymentOption}
+                        name="paymentMethod"
+                        value={paymentOption}
+                        checked={selectedPayment === paymentOption}
+                        onChange={handlePaymentChange}
+                        required
                       />
-                    </label>
-                  </div>
-                )
-              )}
+                      <label htmlFor={paymentOption}>
+                        <img
+                          src={getImageSource(paymentOption)}
+                          alt={paymentOption}
+                        />
+                      </label>
+                    </div>
+                  )
+                )}
+              </div>
             </div>
-          </div>
-        <div className="card-payment2">
-          <strong>Delivery Address</strong>
-          <div className="delivery-address-form">
-            <label htmlFor="area" className="form-label">
-              Area:
-            </label>
-            <select id="fi-cityId" name="cityId" className="form-label">
-              <option value="" disabled="">
-                Please select
-              </option>
-              <option value="1027">* SELECT YOUR CITY / AREA BELOW *</option>
+            <div className="card-payment2">
+              <strong>Delivery Address</strong>
+              <div className="delivery-address-form">
+                <label htmlFor="area" className="form-label">
+                  Area:
+                </label>
+                <select id="fi-cityId" name="cityId" className="form-label">
+                  <option value="" disabled="">
+                    Please select
+                  </option>
+                  <option value="1027">* SELECT YOUR CITY / AREA BELOW *</option>
 
-              {locations.map((location) => (
-                <option key={location.id} value={location.id}>
-                  {location.name}
-                </option>
-              ))}
-            </select>
-            <br />
-            <label htmlFor="street" className="form-label">
-              Street:
-            </label>
-            <input
-              type="text"
-              id="street"
-              name="street"
-              className="form-input"
-              placeholder="Monrovia, UtaliiAve"
-              required
-            />
+                  {locations.map((location) => (
+                    <option key={location.id} value={location.id}>
+                      {location.name}
+                    </option>
+                  ))}
+                </select>
+                <br />
+                <label htmlFor="street" className="form-label">
+                  Street:
+                </label>
+                <input
+                  type="text"
+                  id="street"
+                  name="street"
+                  className="form-input"
+                  placeholder="Monrovia, UtaliiAve"
+                  required
+                />
 
-            <label htmlFor="building" className="form-label">
-              Building:
-            </label>
-            <input
-              type="text"
-              id="building"
-              name="building"
-              className="form-input"
-              placeholder="GTC, Mirage, KICC, Chancery, UAP"
-              required
-            />
-          <label htmlFor="room" className="form-label">
-            Room:
-          </label>
-          <input
-            style={{fontFamily:'Times New Roman', fontSize:"13px"}}
-            type="text"
-            id="room"
-            name="room"
-            className="form-input"
-            placeholder="Room No. House No. Office Name"
-            required
-          />
+                <label htmlFor="building" className="form-label">
+                  Building:
+                </label>
+                <input
+                  type="text"
+                  id="building"
+                  name="building"
+                  className="form-input"
+                  placeholder="GTC, Mirage, KICC, Chancery, UAP"
+                  required
+                />
+                <label htmlFor="room" className="form-label">
+                  Room:
+                </label>
+                <input
+                  style={{fontFamily:'Times New Roman', fontSize:"13px"}}
+                  type="text"
+                  id="room"
+                  name="room"
+                  className="form-input"
+                  placeholder="Room No. House No. Office Name"
+                  required
+                />
 
-            <label htmlFor="notes" className="form-label">
-              Notes:
-            </label>
-            <textarea
-              id="notes"
-              name="notes"
-              className="form-input"
-              placeholder="Anything we should know before entering your property"
-            />
-          </div>
-        </div>
-          <div className="card-total">
-            <strong>Order Summary</strong>
-            <hr />
-            <p>Item's total:</p>
-            <p>Delivery fee:</p>
-            <hr />
-            <p>Total:</p>
-            <hr />
-            <div className="buttons-container">
-              <button type="submit" className="continue-shopping">
-                Pay Now
-              </button>
-              <Link to={'/tracking'}>
-                <button className="continue-shopping">Pay on Delivery</button>
+                <label htmlFor="notes" className="form-label">
+                  Notes:
+                </label>
+                <textarea
+                  id="notes"
+                  name="notes"
+                  className="form-input"
+                  placeholder="Anything we should know before entering your property"
+                />
+              </div>
+            </div>
+            <div className="card-total">
+              <strong>Order Summary</strong>
+              <hr />
+              <p>Item's total:</p>
+              <p>Delivery fee:</p>
+              <hr />
+              <p>Total:</p>
+              <hr />
+              <div className="buttons-container">
+                <button type="submit" className="continue-shopping">
+                  Pay Now
+                </button>
+                <Link to={'/tracking'}>
+                  <button className="continue-shopping">Pay on Delivery</button>
                 </Link>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
+      <Footer />
     </div>
-    <div>{<Footer />}</div>
-  </div>
-);
+  );
 };
 
 const getImageSource = (paymentOption) => {
-switch (paymentOption) {
-  case 'mpesa':
-    return mpesaLogo;
-  case 'cash':
-    return cashLogo;
-  case 'paypal':
-    return paypalLogo;
-  case 'visa':
-    return visaLogo;
-  case 'binance':
-    return binanceLogo;
-  default:
-    return '';
-}
+  switch (paymentOption) {
+    case 'mpesa':
+      return mpesaLogo;
+    case 'cash':
+      return cashLogo;
+    case 'paypal':
+      return paypalLogo;
+    case 'visa':
+      return visaLogo;
+    case 'binance':
+      return binanceLogo;
+    default:
+      return '';
+  }
 };
-
 
 export default PaymentPage;
