9034139ead438b202be9c23504313884b4375b19
maureen <maureen.murimi@moringaschool.com>
2024-07-06 13:32:45 +0300

    Sidebar made

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUtensils } from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+function Sidebar() {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className="sidebar-wrapper">
+      <div className={`menu-icon${isSidebarOpen ? ' open' : ''}`} onClick={toggleSidebar}>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+
+      {isSidebarOpen && (
+        <div className="sidebar">
+          <ul className="sidebar-list">
+            <li>
+              <Link to="/menu">
+                <FontAwesomeIcon icon={faUtensils} className='side-menu' />
+                Menu
+              </Link>
+              <hr className='sidebar-line' />
+            </li>
+            <li>
+              <Link to="/about-us">
+                <FontAwesomeIcon icon={faInfoCircle} className='about-icon'/>
+                About
+              </Link>
+              <hr className='sidebar-line' />
+            </li>
+            <li>
+              <Link to="/contact-us">
+                <FontAwesomeIcon icon={faEnvelope} className='contact-icon'/>
+                Contact Us
+              </Link>
+              <hr className='sidebar-line' />
+            </li>
+            <li>
+              <Link to="/cart">
+                <FontAwesomeIcon icon={faShoppingCart} className='order-icon'/>
+                Orders
+              </Link>
+              <hr className='sidebar-line' />
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Sidebar;
