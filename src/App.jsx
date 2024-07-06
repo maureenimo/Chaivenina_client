e48710bc01382ef8d626ace06b88100b9937d6d0
maureen <maureen.murimi@moringaschool.com>
2024-07-06 13:39:46 +0300

    Debuggging and adding correct routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Menu from './components/Menu';
+import SignIn from './components/SignIn';
+import Register from './components/SignUp';
+import PaymentPage from './components/PaymentPage';
+import TrackingPage from './components/TrackingPage';
+import HomePage from './components/HomePage';
+import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs';
+import Cart from './components/Cart';
+import DineInForm from './components/DineInForm';
+import MpesaPaymentPage from './components/PaymentForm';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cartItems, setCartItems] = useState([]);
+
+  const onAddToCart = (item) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  const removeFromCart = (itemId) => {
+    const updatedCart = cartItems.filter((item) => item.id !== itemId);
+    setCartItems(updatedCart);
+  };
+
+  const handleAddToCart = (item) => {
+    setCartItems([...cartItems, item]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/menu" element={<Menu onAddToCart={onAddToCart} cartItems={cartItems} />} />
+        <Route path="/cart" element={<Cart cartItems={cartItems} removeFromCart={removeFromCart} />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/sign_up" element={<Register />} />
+        <Route path="/sign_in" element={<SignIn />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/tracking" element={<TrackingPage />} />
+        <Route path="/mpesa_payment" element={<MpesaPaymentPage />} /> 
+        <Route path="/dine-in" element={<DineInForm/>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
